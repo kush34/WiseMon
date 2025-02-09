61e8132ec9855d953f6011486e8ee5fc4c019f9a
kush34 <chattkush34@gmail.com>
2025-02-09 20:59:03 +0530

    added Rocket Model in landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import { useNavigate } from 'react-router-dom'
+import SplineScene from './components/SplineScene'
 const App = () => {
   const navigate = useNavigate();
   return (
     <div className='bg-black w-full h-screen text-white'>
-      <div className='p-5'>
+      <div className='p-2'>
         <Navigation/>
       </div>
-      <div className='flex m-15'>
-        <div className="left hero w-1/2 p-10  h-78 text-4xl font-bold flex flex-col justify-center items-center">
-          <div className="text-left">
-            Your Ultimate<br></br>
-            Finance Guide 
-          </div>
-          <div onClick={()=>navigate('/register')} className="cursor-pointer calltoaction font-semibold text-xl bg-green-600 px-4 flex justify-center py-2 rounded mt-5">
-            Get Started
-          </div>
+      <div className='flex flex-col md:flex-row m-10'>
+        <div className='md:w-1/2'>
+          <SplineScene/>
         </div>
-        <div className='w-1/2 '>
-          
+        <div className="md:w-1/2 h-[35vh] md:h-[70vh] flex flex-col justify-center items-center">
+          <div className='flex flex-col text-xl md:text-4xl'> 
+            Your Ultimate 
+            <span className='text-2xl md:text-6xl font-bold'>
+              Finance Guide
+            </span> 
+          </div>
+          <div>
+            <button className='bg-green-600 rounded px-5 py-2 m-5 cursor-pointer'>Get Started</button>
+          </div>
         </div>
       </div>
       <div className="credits flex justify-center items-center text-white font-medium">
---
+++ frontend/src/components/SplineScene.jsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div className="relative w-full h-[40vh] md:h-[70vh]">
+      <Spline className='rounded-xl' scene="https://prod.spline.design/RFhLH1xOCg0edqIa/scene.splinecode" />
+    </div>
+    
+  );
+}
